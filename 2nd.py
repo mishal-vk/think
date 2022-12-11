dbb61cb5a068d200dea00a7bbe31eb8f5d42f4a6
mishal vk <120252076+mishal-vk@users.noreply.github.com>
2022-12-11 10:59:21 +0530

    Add files via upload

---
+++ 2nd.py
@@ -0,0 +1,26 @@
+# name="michu"
+# age=19
+# city="vengara"
+# print(name)
+# print(id(name))
+# print(age)
+# print(id(age))
+# print(city)
+# print(id(city))
+# a=10
+# b=10
+# print(a)
+# print(b)
+# print(id(a))
+# print(id(b))
+# print(age,city,sep="@")
+# a=b=c=10
+# print(a)
+# print(b)
+# print(c)
+# print(a,b,c)
+# a,b,c=999,24.789,"python interpreter"
+# print(a,b,c)
+# print(a)
+# print(b)
+# print(c)
